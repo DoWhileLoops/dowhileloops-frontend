7e9bc9dcaf49f1fc389968654acac9f6d419b41a
Chris Clemons <chrisclemonstech@gmail.com>
2019-11-23 15:48:38 -0800

    add soundcloud iframe

--- src/views/Tunes.vue
+++ src/views/Tunes.vue
@@ -1,5 +1,9 @@
 <template>
-    <h1>Tunes</h1>
+    <div>
+        <h1>Tunes</h1>
+        <!-- can be customized here: https://help.soundcloud.com/hc/en-us/articles/115003568008-Embedding-a-track-or-playlist- -->
+        <iframe width="60%" height="400" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/590946879&color=%23091826&auto_play=false&hide_related=true&show_comments=true&show_user=false&show_reposts=false&show_teaser=true&visual=false"></iframe>
+    </div>
 </template>
 
 <script>
@@ -7,3 +11,12 @@ export default {
     
 }
 </script>
+
+<style>
+/* h1{
+    display: none
+} */
+.multiSoundDetails {
+    display:none
+}
+</style>
