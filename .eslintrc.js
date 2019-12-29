380a9c7fe21ebd13cc63f214c8f505b35982335e
Chris Clemons <chrisclemonstech@gmail.com>
2019-12-28 18:45:32 -0800

    About page revamped, some lint warnings turned off

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,13 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-mixed-spaces-and-tabs': 'off',
+    'indent': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/attributes-order': 'off',
+    'no-trailing-spaces': 'off',
+    'vue/html-indent': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,69 @@
 <template>
-    <v-container fill-height fluid grid-list-xl>
-        <v-layout wrap>
-            <h1>About</h1>
-        </v-layout>
-    </v-container>
+  <v-container
+    fluid
+    grid-list-xl
+    fill-height>
+    <v-layout
+      justify-center
+      align-center
+    >
+      <v-flex xs12>
+        <material-card color="#1276ae">
+            <div slot="header">
+              <div class="title font-weight-light mb-2">
+                DOWHILELOOPS
+            </div>
+            </div>            
+            
+            <v-card-text class="text-xs-center">
+              <v-layout 
+                row
+                wrap
+              >
+              <v-flex 
+              sm12
+              md9
+              >
+                <!-- TODO: Redo this copy -->
+                <p class="card-description font-weight-light">
+                  I'm Chris Clemons, a .NET developer based in Seattle. I've been programming professionally since 2016, and before that, I was a performing musician.
+                </p>
+                <p class="card-description font-weight-light">
+                  DoWhileLoops is meant to showcase work that I have done in both fields, as well as posts that will hopefully inform and entertain fellow enthusiasts.
+                </p>
+                <p class="card-description font-weight-light">
+                  Please feel free to email me, or check out my LinkedIn for more details.
+                </p>                
+              </v-flex>
+              <v-flex
+              sm12
+              md3>
+                <v-avatar
+                slot="offset"
+                class="mx-auto d-block"
+                size="200"
+                >
+                <img
+                  src="../assets/corporateMug.png"
+                >
+                </v-avatar>
+              </v-flex>
+              </v-layout>
+            <!-- <v-btn
+              color="success"
+              round
+              class="font-weight-light"
+            >Follow</v-btn> -->
+          </v-card-text>
+        </material-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
 
 <script>
 export default {
-    
+    name: 'About',
 }
 </script>
