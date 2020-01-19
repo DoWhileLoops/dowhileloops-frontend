bb2bd452a6a44a5a7018ad1a24a19968313464e2
Chris Clemons <chrisclemonstech@gmail.com>
2020-01-19 09:13:17 -0800

    404 page styling 2

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -34,7 +34,7 @@ export default {
     name: 'NotFound',
     methods: {
         bringItOnHome () {
-            this.$router.push({ name:  'Home'})
+            this.$router.push({ name: 'Home'})
         }
     }
 }
